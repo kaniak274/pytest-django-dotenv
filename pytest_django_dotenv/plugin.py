6b08c9cb472589c476ff7cd906e3ef466fc78ec6
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-11-26 13:22:45 +0100

    Add missing ..

--- pytest_django_dotenv/plugin.py
+++ pytest_django_dotenv/plugin.py
@@ -6,5 +6,5 @@ import pytest
 @pytest.hookimpl(tryfirst=True)
 def pytest_load_initial_conftests(args, early_config, parser):
     virtual_env_path = os.getenv('VIRTUAL_ENV')
-    dotenv.read_dotenv(os.path.join(virtual_env_path, '.env'))
+    dotenv.read_dotenv(os.path.join(virtual_env_path, '../.env'))
 
