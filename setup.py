ef555db82b5f48c1277db054cc54d74a40848d54
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-11-26 13:01:54 +0100

    Fix missing , in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='pytest-django-dotenv',
-    version='0.1.0'
-    author='Kamil Kucharski'
+    version='0.1.0',
+    author='Kamil Kucharski',
     author_email='kaniak274@gmail.com',
     packages=['pytest_django_dotenv'],
-    entry_points={'pytest11': ['env = pytest_django_dotenv.plugin']}
+    entry_points={'pytest11': ['env = pytest_django_dotenv.plugin']},
     install_requires=['pytest>=2.6.0', 'django-dotenv>=1.4.2'],
     classifiers=[
         'Development Status :: 4 - Beta',
