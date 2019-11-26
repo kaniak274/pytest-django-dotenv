5b24fa92095974ab13bf3f467a877d05d3c4ec93
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-11-26 13:43:29 +0100

    Add addoption

--- pytest_django_dotenv/plugin.py
+++ pytest_django_dotenv/plugin.py
@@ -3,8 +3,16 @@ import dotenv
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addini(
+        "env_path",
+        type="linelist",
+        help="path to .env from $VIRTUAL_ENV"
+        default=['.env']
+    )
+
 @pytest.hookimpl(tryfirst=True)
 def pytest_load_initial_conftests(args, early_config, parser):
     virtual_env_path = os.getenv('VIRTUAL_ENV')
-    dotenv.read_dotenv(os.path.join(virtual_env_path, '../.env'))
+    dotenv.read_dotenv(os.path.join(virtual_env_path, f'../{early_config.getini("env_path")[0]}'))
 
