fc8cc8da546ba7ff9132d445f3547d4420e9fc14
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-11-26 13:09:38 +0100

    Change plugin

--- pytest_django_dotenv/plugin.py
+++ pytest_django_dotenv/plugin.py
@@ -1,7 +1,9 @@
+import os
 import dotenv
 import pytest
 
+
 @pytest.hookimpl(tryfirst=True)
 def pytest_load_initial_conftests(args, early_config, parser):
-    dotenv.read_dotenv()
+    dotenv.read_dotenv(os.path.join(os.path.dirname(__file__), '../../../../../.env'))
 
