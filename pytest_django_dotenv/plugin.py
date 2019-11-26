ad9c9250cd36f94a1720eb39ccbdee97e6a56707
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-11-26 13:21:47 +0100

    Use VIRTUAL_ENV variable

--- pytest_django_dotenv/plugin.py
+++ pytest_django_dotenv/plugin.py
@@ -5,5 +5,6 @@ import pytest
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_load_initial_conftests(args, early_config, parser):
-    dotenv.read_dotenv(os.path.join(os.path.dirname(__file__), '../../../../../.env'))
+    virtual_env_path = os.getenv('VIRTUAL_ENV')
+    dotenv.read_dotenv(os.path.join(virtual_env_path, '.env'))
 
